90d8d0ff80ed0d5fb810eb158676899a50f36183
saadhn4 <saadcfi4@gmail.com>
2024-12-19 13:43:39 +0530

    wrote get all and get one apis

--- server/controllers/users/index.js
+++ server/controllers/users/index.js
@@ -24,4 +24,39 @@ router.post("/register", async (req, res) => {
   }
 });
 
+//now im doing the apis in the steps
+
+router.get("/getall", async (req, res) => {
+  try {
+    let allUsers = await userModel.find({});
+    console.log(allUsers);
+    res.status(200).json(allUsers);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/getone", async (req, res) => {
+  try {
+    let user = await userModel.find({ firstName: "Saad" });
+    console.log(user);
+    res.status(200).json({ user: user });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+//this i did on my own
+
+router.get("/count", async (req, res) => {
+  try {
+    let result = await userModel.countDocuments({});
+    console.log(result);
+    res.status(200).json({ count: result });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: error });
+  }
+});
+
 export default router;
